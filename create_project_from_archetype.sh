116dab59bf7121b88842be01f1c2ea7bac7fdb01
acasolla <casolla.alessandro@gmail.com>
2014-01-28 15:39:03 +0100

    1.0

--- create_project_from_archetype.sh
+++
@@ -1,8 +0,0 @@
-mvn -Psoftphone archetype:generate \
--DgroupId=it.softphone \
--DartifactId=commons-gwt-showcase \
--Dversion=1.0-SNAPSHOT \
--DarchetypeGroupId=it.soulsoftware \
--DarchetypeArtifactId=gwtp-archetype \
--DarchetypeVersion=1.0 \
--DremoteRepositories=http://support.softphone.it/artifactory/private-internal-repository
